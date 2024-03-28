6144390e153f6962ed898b1e2b70cafa35bd1813
Mensh Nikolay <tony.borbozo@gmail.com>
2024-03-28 22:19:22 +0500

    registration is ready

--- drf_jwt/drf_app/models.py
+++ drf_jwt/drf_app/models.py
@@ -1,12 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser,Permission
 
 
 
-class User(AbstractUser):
-        user_permissions = models.ManyToManyField(Permission, blank=True, related_name='drf_users_permissions',)
-        groups = models.ManyToManyField(
-        'auth.Group',
-        blank=True,
-        related_name='custom_user_set',  # Или любое другое подходящее имя
-    )
+class RefCode(models.Model):
+    
--- drf_jwt/drf_app/serializers.py
+++ drf_jwt/drf_app/serializers.py
@@ -1,15 +1,14 @@
 from rest_framework import serializers
-from drf_app.models import User
+from django.contrib.auth.models import User
+#from drf_app.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username', 'email', 'password','password2']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password']
+        
 
     def create(self, validated_data):
-        password = validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
-        user.set_password(password)
-        user.save()
         return user
